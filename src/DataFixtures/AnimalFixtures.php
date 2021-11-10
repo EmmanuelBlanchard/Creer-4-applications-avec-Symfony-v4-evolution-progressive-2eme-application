dadb2489e4ca35f0acf345729ea07240d910a57a
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-10 21:36:49 +0100

    La deuxieme application, 13-Personnes - Relation 1.n - 1.n avec propriete

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -4,7 +4,9 @@ namespace App\DataFixtures;
 
 use App\Entity\Animal;
 use App\Entity\Continent;
+use App\Entity\Dispose;
 use App\Entity\Famille;
+use App\Entity\Personne;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -12,22 +14,28 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $p1 = new Personne();
+        $p1->setNom("Milo");
+        $manager->persist($p1);
+        $p2 = new Personne();
+        $p2->setNom("Tya");
+        $manager->persist($p2);
+        $p3 = new Personne();
+        $p3->setNom("Lili");
+        $manager->persist($p3);
+
         $continent1 = new Continent();
         $continent1->setLibelle("Europe");
         $manager->persist($continent1);
-
         $continent2 = new Continent();
         $continent2->setLibelle("Asie");
         $manager->persist($continent2);
-
         $continent3 = new Continent();
         $continent3->setLibelle("Afrique");
         $manager->persist($continent3);
-
         $continent4 = new Continent();
         $continent4->setLibelle("Océanie");
         $manager->persist($continent4);
-
         $continent5 = new Continent();
         $continent5->setLibelle("Amérique");
         $manager->persist($continent5);
@@ -59,7 +67,6 @@ class AnimalFixtures extends Fixture
             ->addContinent($continent5)
         ;
         $manager->persist($a1);
-
         $a2 = new Animal();
         $a2->setNom("Cochon")
             ->setDescription("Un animal d'élevage")
@@ -71,7 +78,6 @@ class AnimalFixtures extends Fixture
             ->addContinent($continent5)
         ;
         $manager->persist($a2);
-
         $a3 = new Animal();
         $a3->setNom("Serpent")
             ->setDescription("Un animal dangereux")
@@ -84,7 +90,6 @@ class AnimalFixtures extends Fixture
             ->addContinent($continent4)
         ;
         $manager->persist($a3);
-
         $a4 = new Animal();
         $a4->setNom("Crocodile")
             ->setDescription("Un animal très dangereux")
@@ -96,7 +101,6 @@ class AnimalFixtures extends Fixture
             ->addContinent($continent4)
         ;
         $manager->persist($a4);
-
         $a5 = new Animal();
         $a5->setNom("Requin")
             ->setDescription("Un animal marin très dangereux")
@@ -108,6 +112,37 @@ class AnimalFixtures extends Fixture
             ->addContinent($continent5)
         ;
         $manager->persist($a5);
+
+        $d1 = new Dispose();
+        $d1->setPersonne($p1)
+            ->setAnimal($a1)
+            ->setNb(30);
+        $manager->persist($d1);
+        $d2 = new Dispose();
+        $d2->setPersonne($p1)
+            ->setAnimal($a2)
+            ->setNb(10);
+        $manager->persist($d2);
+        $d3 = new Dispose();
+        $d3->setPersonne($p1)
+            ->setAnimal($a3)
+            ->setNb(2);
+        $manager->persist($d3);
+        $d4 = new Dispose();
+        $d4->setPersonne($p2)
+            ->setAnimal($a3)
+            ->setNb(5);
+        $manager->persist($d4);
+        $d5 = new Dispose();
+        $d5->setPersonne($p2)
+            ->setAnimal($a4)
+            ->setNb(10);
+        $manager->persist($d5);
+        $d6 = new Dispose();
+        $d6->setPersonne($p3)
+            ->setAnimal($a5)
+            ->setNb(20);
+        $manager->persist($d6);
 
         $manager->flush();
 
